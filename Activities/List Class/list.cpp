31cd6f064c57c4732eeb1be6261c7665d0f2a25f
RenanGAS <renansakaoki@gmail.com>
2022-03-15 09:23:30 -0300

    feat: do some methods of List Class

--- Activities/List Class/list.cpp
+++ Activities/List Class/list.cpp
@@ -39,7 +39,18 @@ class Lista
 private:
     bool validacao(Lista *l)
     {
-        return l != NULL ? true : false;
+        if (l == NULL)
+        {
+            printf("\nErro: Lista inválida.\n");
+            return false;
+        }
+
+        return true;
+    }
+
+    bool posicaoValida(int pos)
+    {
+        return pos >= qtde || pos < 0 ? false : true;
     }
 
     // Métodos principais
@@ -49,10 +60,26 @@ public:
     {
         Lista *nova_lista = new Lista();
 
+        if (!nova_lista->validacao(nova_lista))
+        {
+            printf("\nErro: Não foi possível criar a Lista.\n");
+            return NULL;
+        }
+
+        printf("\nLista criada.\n");
+
         return nova_lista;
     }
 
-    void destruir(Lista **l);
+    void destruir()
+    {
+        delete this;
+        qtde = 0;
+        inicio = NULL;
+        fim = NULL;
+
+        printf("\nLista destruída.\n");
+    }
 
     bool anexar(Lista *l, TipoElemento elemento)
     {
@@ -63,7 +90,7 @@ public:
 
         No *no = no->criar(elemento);
 
-        if (l->vazia(l))
+        if (l->vazia())
         {
             l->inicio = no;
         }
@@ -76,23 +103,128 @@ public:
         l->fim = no;
         qtde++;
 
+        printf("\nElemento %d anexado.\n", elemento);
+
         return true;
     }
 
-    bool inserir(Lista *l, TipoElemento elemento, int posicao);
+    bool inserir(Lista *l, TipoElemento elemento, int posicao)
+    {
+        // int i = 0;
+
+        // for (No *aux = l->inicio; aux != NULL; aux = aux->proximo)
+        // {
+        //     if (i == posicao)
+        //     {
+        //         aux->valor = elemento;
+        //     }
+
+        //     i++;
+        // }
+
+        // printf("\nElemento %d foi inserido com sucesso.\n", elemento);
+
+        return true;
+    }
+
     bool removerPosicao(Lista *l, int posicao, TipoElemento *endereco);
     int removerElemento(Lista *l, TipoElemento elemento);
-    bool substituir(Lista *l, int posicao, TipoElemento novoElemento);
-    int posicao(Lista *l, TipoElemento elemento);
-    bool buscar(Lista *l, int posicao, TipoElemento *endereco);
-    int tamanho(Lista *l);
 
-    bool vazia(Lista *l)
+    bool substituir(Lista *l, int posicao, TipoElemento novoElemento)
+    {
+        if (!l->posicaoValida(posicao))
+        {
+            return false;
+        }
+
+        int i = 0;
+
+        for (No *aux = l->inicio; aux != NULL; aux = aux->proximo)
+        {
+            if (i == posicao)
+            {
+                aux->valor = novoElemento;
+            }
+
+            i++;
+        }
+
+        printf("\nSubstituição na posição %d por %d.\n", posicao, novoElemento);
+
+        return true;
+    }
+
+    bool posicao(Lista *l, TipoElemento elemento)
+    {
+        if (!l->validacao(l))
+        {
+            return -1;
+        }
+
+        int i = 0;
+
+        for (No *aux = l->inicio; aux != NULL; aux = aux->proximo)
+        {
+            if (aux->valor == elemento)
+            {
+                printf("\nElemento %d na posição %d.\n", elemento, i);
+                return true;
+            }
+
+            i++;
+        }
+
+        printf("\nErro: Elemento não encontrado.\n");
+        return false;
+    }
+
+    bool buscar(Lista *l, int posicao, TipoElemento *endereco)
+    {
+        if (!l->validacao(l) || !l->posicaoValida(posicao))
+        {
+            printf("\nErro: Dados inválidos.\n");
+            return false;
+        }
+
+        int i = 0;
+
+        for (No *aux = l->inicio; aux != NULL; aux = aux->proximo)
+        {
+            if (i == posicao)
+            {
+                *endereco = aux->valor;
+                printf("\nA posição %d contém o elemento %d.\n", i, *endereco);
+                return true;
+            }
+
+            i++;
+        }
+
+        return false;
+    }
+
+    int tamanho(Lista *l)
+    {
+        if (!l->validacao(l))
+        {
+            return -1;
+        }
+
+        return qtde;
+    }
+
+    bool vazia()
     {
         return qtde == 0 ? true : false;
     }
+
     bool toString(Lista *l, char *str)
     {
+        if (!l->validacao(l))
+        {
+            return false;
+        }
+
         char aux_char[100];
 
         str[0] = '\0';
@@ -124,17 +256,35 @@ int main()
     l->anexar(l, 30);
     l->anexar(l, 40);
     l->anexar(l, 50);
+    l->substituir(l, 4, 45);
+
+    l->posicao(l, 30);
 
     char str[100];
 
     if (l->toString(l, str))
     {
-        printf("\n%s\n\n", str);
+        printf("\nLista = %s\n", str);
     }
     else
     {
-        printf("\nNão foi possível imprimir a lista.\n\n");
+        printf("\nErro: Não foi possível imprimir a lista.\n");
     }
+
+    if (l->tamanho(l) == -1)
+    {
+        printf("\nErro: Lista inválida.\n");
+    }
+    else
+    {
+        printf("\nTamanho = %d\n", l->tamanho(l));
+    }
+
+    int elem;
+
+    l->buscar(l, 4, &elem);
+
+    l->destruir();
 
     return 0;
 }
